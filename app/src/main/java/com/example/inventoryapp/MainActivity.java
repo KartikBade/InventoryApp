5a64ba5f607085a09fc44edc0505b20668c58653
Kartik Bade <kartikbade19@gmail.com>
2022-08-15 14:04:38 +0530

    Added Loader for database calls

--- app/src/main/java/com/example/inventoryapp/MainActivity.java
+++ app/src/main/java/com/example/inventoryapp/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.inventoryapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.CursorLoader;
+import androidx.loader.content.Loader;
+
 import android.content.Intent;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
@@ -15,21 +20,23 @@ import android.widget.TextView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import data.ProductContract.ProductEntry;
-import data.ProductDbHelper;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private FloatingActionButton fab;
     private ListView mainList;
+    private TextView emptyView;
     private ProductAdapter productAdapter;
+    private static final int PRODUCT_LOADER= 0;
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        emptyView = findViewById(R.id.empty_view);
         mainList = findViewById(R.id.main_list);
+        mainList.setEmptyView(emptyView);
 
         fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -40,26 +47,32 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        displayDbInfo();
+        productAdapter = new ProductAdapter(this, null);
+        mainList.setAdapter(productAdapter);
+
+        getSupportLoaderManager().initLoader(PRODUCT_LOADER, null, this);
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
+    @NonNull
     @Override
-    protected void onStart() {
-        super.onStart();
-        displayDbInfo();
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    private void displayDbInfo() {
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
         String[] projection = {
                 ProductEntry._ID,
                 ProductEntry.COLUMN_PRODUCT_NAME,
                 ProductEntry.COLUMN_PRODUCT_PRICE,
                 ProductEntry.COLUMN_PRODUCT_QUANTITY
         };
-        Cursor cursor = getContentResolver().query(ProductEntry.CONTENT_URI, projection, null, null);
-        productAdapter = new ProductAdapter(this, cursor);
-        mainList.setAdapter(productAdapter);
+
+        return new CursorLoader(this, ProductEntry.CONTENT_URI, projection, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+        productAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+        productAdapter.swapCursor(null);
     }
 }
